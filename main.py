ad00256cb9dbcf9b2816abc201c75e2f718d44a5
egluna-dev <egluna@live.ca>
2022-08-22 16:17:02 +1000

    Created ball class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 # from scoreboard import Scoreboard
 
 screen = Screen()
@@ -8,15 +10,20 @@ screen.setup(width=800, height=600)
 screen.title("Pong Classic")
 screen.tracer(0)
 
-paddle = Paddle()
-paddle.goto(350, 0)
+left_paddle = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
 
+ball = Ball()
 
 screen.listen()
-screen.onkey(key="Up", fun=paddle.up)
-screen.onkey(key="Down", fun=paddle.down)
+screen.onkey(key="Up", fun=right_paddle.up)
+screen.onkey(key="Down", fun=right_paddle.down)
+screen.onkey(key="w", fun=left_paddle.up)
+screen.onkey(key="s", fun=left_paddle.down)
 
 program_on = True
 while program_on:
+    time.sleep(0.05)
     screen.update()
+    ball.move_ball()
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -2,19 +2,21 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
-        super().__init__
-        self.coords = ()
+    def __init__(self, coords):
+        super().__init__()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
+        self.goto(coords)
+        self.up()
+        self.down()
 
     def set_coords(self):
         pass
 
     def up(self):
-        new_y_pos = self.paddle.ycor() + 20
+        new_y_pos = self.ycor() + 20
         self.goto(self.xcor(), new_y_pos)
 
     def down(self):
